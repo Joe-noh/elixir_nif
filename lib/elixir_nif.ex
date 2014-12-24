ddb1cbc433e3822f9f14f24d83f58e98802a527d
Joe-noh <goflb.jh@gmail.com>
2014-12-25 04:43:53 +0900

    implement functions which make basic type objects

--- lib/elixir_nif.ex
+++ lib/elixir_nif.ex
@@ -5,5 +5,11 @@ defmodule ElixirNif do
     :ok = :erlang.load_nif('priv/elixir_nif', 0)
   end
 
-  def integer_one, do: exit(:nif_not_loaded)
+  def atom,    do: exit(:nif_not_loaded)
+  def binary,  do: exit(:nif_not_loaded)
+  def string,  do: exit(:nif_not_loaded)
+  def double,  do: exit(:nif_not_loaded)
+  def integer, do: exit(:nif_not_loaded)
+  def list,    do: exit(:nif_not_loaded)
+  def tuple,   do: exit(:nif_not_loaded)
 end
